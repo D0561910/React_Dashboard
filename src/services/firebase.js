f072f259ddfc3f9c876efe76e270387f10d1cae6
Charles Sin.C.Y <38834820+D0561910@users.noreply.github.com>
2021-04-08 15:05:16 +0800

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,9 +4,6 @@ const config = {
   // apiKey: "ADD-YOUR-DETAILS-HERE",
   // authDomain: "ADD-YOUR-DETAILS-HERE",
   // databaseURL: "ADD-YOUR-DETAILS-HERE",
-  apiKey: "AIzaSyDy51uUpj_x93D6JrkjhYKuj8NKbk4NLCU",
-  authDomain: "chatty-0405.firebaseapp.com",
-  databaseURL: "https://chatty-0405.firebaseio.com",
 };
 firebase.initializeApp(config);
 
